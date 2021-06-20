a7a5660525f875336501494f1a9000b87194fa07
veikkahonkanen <49862032+veikkahonkanen@users.noreply.github.com>
2021-06-20 19:31:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) => {
-  res.send(200, "Hei siellä!");
+  res.send(200, "<!Doctype html><html><head></head><body><h1>Heipat</h1></body></html>");
 })
 
 app.listen(port, ()  => {
-  console.log(`Palvelin käynnissä portissa ${port}`);
+  console.log(`Running at ${port}`);
 });
